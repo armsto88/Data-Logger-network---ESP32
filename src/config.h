7a66e96e34abe1f5ff34ce5c9f192fbb1cd3e561
armsto88 <115899336+armsto88@users.noreply.github.com>
2025-11-03 20:32:21 +0100

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define LED_PIN     48      // Onboard LED pin, update for your board
+#define SD_CS_PIN   10      // SD card Chip Select
+#define SD_MOSI     11
+#define SD_MISO     13
+#define SD_SCK      12
+
+#define RTC_SDA     42      // DS3231 SDA
+#define RTC_SCL     41      // DS3231 SCL
+#define RTC_INT_PIN 3       // DS3231 INT/SQW out
+
+#define ESPNOW_CHANNEL 1    // ESP-NOW WiFi channel
+
+// Add more device/node IDs as needed here!
