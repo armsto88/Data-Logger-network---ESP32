8648836aa06801623bc4c685a10f316939daefc1
armsto88 <115899336+armsto88@users.noreply.github.com>
2025-11-04 07:45:38 +0100

    i2n rtc

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define SD_CS_PIN   10
-#define RTC_SDA     8   // Use GPIO 8 for SDA (confirmed working on ESP32-S3)
-#define RTC_SCL     9   // Use GPIO 9 for SCL (confirmed working on ESP32-S3)
+#define RTC_SDA     4   // GPIO 4 for SDA 
+#define RTC_SCL     5   // GPIO 5 for SCL
 #define RTC_INT_PIN 3
 #define ESPNOW_CHANNEL 1
